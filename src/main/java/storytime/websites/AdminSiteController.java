282889f84cb7d3c45594fd2f2c127d3ebf2ae2ae
jkasato <jasato120490@gmail.com>
2020-04-17 22:32:38 -0700

    temp save admin page
     im temporarily saving my code in this branch, dont merge into anything

--- src/main/java/storytime/websites/AdminSiteController.java
+++ src/main/java/storytime/websites/AdminSiteController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class AdminSiteController {
     Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
+    //http://localhost:8090/admin/stories
     @GetMapping("/admin/stories")
     public String stories(ModelMap model) {
         log.info("getting /admin/stories");
         return "admin/story-all-show";
     }
-
+    
+    //http://localhost:8090/admin/story/edit/1
     @GetMapping("/admin/story/edit/{id}")
     public String story__edit(ModelMap model,
                               @PathVariable("id") long id) {
@@ -27,10 +29,12 @@ public class AdminSiteController {
         return "admin/story-one-edit";
     }
 
+    //http://localhost:8090/admin/story/new
     @GetMapping("/admin/story/new")
     public String story__new(ModelMap model) {
         log.info("getting admin/story/new");
         return "admin/story-one-edit";
+//        return "admin/story-one-new";
     }
 
 }
