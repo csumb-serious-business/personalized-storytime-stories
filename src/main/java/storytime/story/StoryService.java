60af6998656e543bced0cb074ab47ad30e6c6606
jkasato <jasato120490@gmail.com>
2020-04-19 02:33:00 -0700

    need help
    added buttons for the admin panel to add/edit stories
    addedform to add story content and title
    added window to show completed story

--- src/main/java/storytime/story/StoryService.java
+++ src/main/java/storytime/story/StoryService.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import storytime.child.Child;
+
 import java.util.Optional;
 
 @Service
@@ -42,4 +44,12 @@ public class StoryService {
     return true;
   }
 
+    public boolean persist(Story story) {
+        try {
+            repo.save(story);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
--- src/main/java/storytime/websites/AdminSiteController.java
+++ src/main/java/storytime/websites/AdminSiteController.java
@@ -1,35 +1,81 @@
 package storytime.websites;
 
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import storytime.child.Child;
+import storytime.story.Story;
+import storytime.story.StoryService;
 
 @Controller
 @EnableAutoConfiguration
 public class AdminSiteController {
-  Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
+	Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
-  @GetMapping("/admin/stories")
-  public String stories(ModelMap model) {
-    log.info("getting /admin/stories");
-    return "admin/story-all-show";
-  }
+	@Autowired
+	StoryService storyService;
 
-  @GetMapping("/admin/story/edit/{id}")
-  public String story__edit(ModelMap model, @PathVariable("id") long id) {
-    log.info("getting /admin/story/edit/{}", id);
-    model.addAttribute("id", id);
-    return "admin/story-one-edit";
-  }
+	@GetMapping("/admin/stories")
+	public String stories(Model model) {
+		log.info("getting /admin/stories");
+		return "admin/story-all-show";
+	}
 
-  @GetMapping("/admin/story/new")
-  public String story__new(ModelMap model) {
-    log.info("getting admin/story/new");
-    return "admin/story-one-edit";
-  }
+	@GetMapping("/admin/story/edit/{id}")
+	public String story__edit(ModelMap model, @PathVariable("id") long id) {
+		log.info("getting /admin/story/edit/{}", id);
+		model.addAttribute("id", id);
+		return "admin/story-one-edit";
+	}
+
+	@GetMapping("/admin/story/new")
+	public String story__new(ModelMap model) {
+		log.info("GET admin/story/new");
+		model.addAttribute("story", new Story());
+		return "admin/story-one-new";
+	}
+
+	@PostMapping("/admin/story/new")
+	public String story__new(@Valid @ModelAttribute("story") Story story, BindingResult result) {
+		if (result.hasErrors()) {
+			return "admin/story-one-new";
+		}
+		log.info("POST /admin/story/new -- story");
+		String url = "/admin/story/" + story.getId();
+		log.info("redirecting to: {}", url);
+		return "redirect:" + url;
+	}
+
+	@GetMapping("/admin/story/{id}")
+	public String admin__id__new(Model model, @PathVariable("id") long id) {
+		log.info("GET /admin/story/{}", id);
+		model.addAttribute("story", new Story());
+		return "admin/story-show";
+	}
+
+	@PostMapping("/admin/story/{id}")
+	public String admin__id__new(Model model, @PathVariable("id") long id, @Valid @ModelAttribute("story") Story story,
+			BindingResult result) {
+		storyService.getStoryById(id).ifPresent(model::addAttribute);
+		if (result.hasErrors()) {
+			log.info("result: {}", result);
+			return "admin/story-all-show";
+		}
+		log.info("POST /admin/story/{} -- story {}", id, story);
+		storyService.persist(story);
+		return stories(model);
+	}
 
 }
