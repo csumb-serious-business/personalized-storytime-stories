7c16bf329f8948eead7b21ff31f673af88ce5562
jkasato <jasato120490@gmail.com>
2020-04-18 19:53:32 -0700

    after mysql is set up, added admin site

--- src/main/java/storytime/websites/AdminSiteController.java
+++ src/main/java/storytime/websites/AdminSiteController.java
@@ -13,12 +13,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class AdminSiteController {
     Logger log = LoggerFactory.getLogger(this.getClass().getSimpleName());
 
+  //http://localhost:8090/admin/stories
     @GetMapping("/admin/stories")
     public String stories(ModelMap model) {
         log.info("getting /admin/stories");
         return "admin/story-all-show";
     }
-
+    
+  //http://localhost:8090/admin/story/edit/1
     @GetMapping("/admin/story/edit/{id}")
     public String story__edit(ModelMap model,
                               @PathVariable("id") long id) {
@@ -26,11 +28,13 @@ public class AdminSiteController {
         model.addAttribute("id", id);
         return "admin/story-one-edit";
     }
-
+    
+    //http://localhost:8090/admin/story/new
     @GetMapping("/admin/story/new")
     public String story__new(ModelMap model) {
         log.info("getting admin/story/new");
-        return "admin/story-one-edit";
+//        return "admin/story-one-edit";
+      return "admin/story-one-new";
     }
 
 }
