71686d88601a8a70ec30a721581c468a80538dd9
Raymond Talmage <raymondtalmage@gmail.com>
2020-04-21 19:54:16 -0700

    Load stories into SQL db on startup

---
+++ src/main/java/storytime/story/StoryLoad.java
@@ -0,0 +1,22 @@
+package storytime.story;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class StoryLoad {
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
+        ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
+        resourceDatabasePopulator.addScript(new ClassPathResource("/import.sql"));
+        DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
+        dataSourceInitializer.setDataSource(dataSource);
+        dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
+        return dataSourceInitializer;
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,393 @@
+insert into story values(1, 'I Want a Monkey', '${protagonist} was sure that he wanted a pet.
+
+He talked to ${dad} about which pet to get.
+
+
+
+“I want a Monkey who scratches his head!
+
+He’d swing in the house and he’d jump on the bed.”
+
+
+
+“Well,” said ${dad} in a kind sort of way,
+
+“Did you know that monkeys eat bugs everyday?”
+
+
+
+“Oh No,” ${protagonist} said, “We have none to eat.”
+
+${sister} likes to squash all the bugs with her feet.”
+
+
+
+“Then I want a Hippo!” he screamed to ${dad}.
+
+“He’d eat all our trash, and ${mom} would be glad!”
+
+
+
+“Hippos need water, it helps them stay cool.
+
+I guess we can build him a giant sized pool.”
+
+
+
+“No,” ${protagonist} said, “he’d be too big to dry,
+
+We’d need a mountain of towels to try.”
+
+
+
+“I want an Otter who triple back flips,
+
+I’d practice swimming when we go for dips.”
+
+
+
+“Did you know most otters can whistle and chirp?
+
+They growl and snort, and perhaps even burp?”
+
+
+
+“Oh,” ${protagonist} said with a sad looking frown,
+
+“We’d have to tell him to keep the noise down.”
+
+
+
+“Let’s get a Giraffe!” ${protagonist} said with a smile.
+
+“I’d slide from his neck to a giant clothes pile!”
+
+
+
+“Giraffes hold the record for tallest of all.”
+
+“The jump from their head is a twenty foot fall!”
+
+“Gosh,” ${protagonist} said as he looked at the ceiling,
+
+He’d bump his head even if he were kneeling.”
+
+
+
+“Well how ‘bout a bunny?” he said to ${dad},
+
+“A fluffy and floppy one can’t be that bad.”
+
+
+
+“Bunnies like eating and chew all your stuff.
+
+ Raising a bunny can really be tough.”
+
+
+
+“Wow,” ${protagonist} answered, “Is that really true?
+
+Picking a pet is not easy to do.”
+
+
+
+“I want a ${pet-species}!” ${protagonist} said, “Let’s get one!”
+
+“I think a ${pet-species} could be lots of fun.”
+
+
+
+“Hmm,” said ${dad}, with a look of delight.
+
+“Good idea ${protagonist}; I think that you’re right!”
+
+
+
+“They’re really good swimmers and make funny faces.
+
+And if we get two, we could watch them have races!”
+
+
+
+“Okay,” said ${dad}, “That’s just what we’ll do.
+
+One ${pet-species} for me, one ${pet-species} for you!”');
+
+insert into story values(2, 'The Friendly Girrafe', 'Once upon a time, there was a giraffe named
+${protagonist}. ${protagonist} lived in Kenya, a country in
+Africa. Like all giraffes, ${protagonist} had a long neck
+and long legs. Because she was so tall, she was
+able to eat food from the very tops of the trees in
+the savannah. The savannah in Africa is an area
+with lots of grass and some trees. Sometimes, a
+savannah is called grasslands. The other animals
+like zebras and ${pet-species} could not reach where
+${protagonist} could reach. But ${protagonist} always found
+food. She loved the leaves and the new buds of the
+trees.
+
+One day, ${protagonist} was busy eating her favorite
+leaves along with some other giraffes. It was a
+bright sunny day and there was not a cloud in the
+sky. It had not rained for a long time, so the grass
+was very dry. She heard a noise down at the
+bottom of her extra long legs: it was her friend,
+${pet} the ${pet-species}. ${pet} was trying to say
+something, but ${protagonist} couldn’t understand what
+he was saying. He looked very tired. “What’s
+wrong?” asked ${protagonist}. ${protagonist} was a very kind
+giraffe and wanted to help everyone.
+Just then ${pet} fell over!
+
+${protagonist} was also a smart giraffe. She knew what
+was wrong. ${pet} couldn’t find anything to eat
+and he was very hungry. ${protagonist} nibbled some
+fresh, tender leaves and buds from the top of the
+tree where she was eating. She dropped some
+of the leaves and buds to the ground near where
+${pet} was. Then she nudged him a little with
+the hoof at the bottom of one of her very long legs.
+“Wake up, ${pet}!” said ${protagonist}.
+“I’ve found something for you to eat.”
+Slowly ${pet} sat up and ate a little.
+“Thank you, ${protagonist},” said ${pet}.
+
+After a while, when ${pet} was feeling better,
+${protagonist} asked him, “What’s happened, ${pet}?
+Why are you so hungry? Why can’t you find
+anything to eat?” ${pet} said, “It hasn’t
+rained for so long and now there’s no more
+food.” “That’s not good,” said ${protagonist}. “What’s
+happening with the other animals?” “No one
+knows what to do,” answered ${pet}. “All the
+zebras and the ${pet-species} and the elephants are
+getting worried about the dry grass.
+Some of them are thinking about leaving the
+savannah and trying to reach the jungle.”
+“That’s a long trip,” said ${protagonist}.
+“Are you going to go with them?”
+
+“I don’t know,” answered ${pet}. “What do
+you think we should do?” ${protagonist} thought for a
+while and then suddenly, she had a good idea. “We
+should go talk to ${dad} the Lion. He is the smartest
+animal in the savannah!” ${pet} was too tired to
+walk, so ${protagonist} invited him to ride on her back.
+“Hold on tight to my neck,” said ${protagonist}. “There’s
+a lot to hold on to,” joked ${pet}. ${protagonist}
+laughed. “You’re right. I’ve got the longest neck
+in Africa!” So they started their journey across to
+the other side of the savannah to look for ${dad}.
+
+${protagonist} and ${pet} were in luck! ${dad} was sitting
+up on a rock and he was really easy to see as they
+walked through the grass. “Hi, ${dad}!” they both
+shouted. ${dad} had been napping and was a little
+grumpy when they woke him up. But he was
+polite and said, “Hello ${protagonist} and ${pet}. Why
+did you walk the whole way across the savannah?
+Just to wake me up?” But he was joking; he had
+a smile on his face. “${pet} says that there is
+no food left on the savannah because it’s been
+so dry,” said ${protagonist}. “Do you have any ideas?”
+
+${dad} thought for a while. He said, “There’s nothing
+we can do about the rain. It will come soon, but
+we can’t be sure when it will arrive. When the
+rain comes, all the plants will grow again and
+there will be lots of food for everybody.”
+He thought a little longer. “The only thing I can
+think of is for everyone to move closer to the
+forest,” he continued. “There will be more things
+to eat there, but I know it’s a long trip and it will
+not be very easy. It might take days to get there.”
+
+${protagonist} and ${pet} looked at each other and
+then looked back at ${dad}. “Thank you, ${dad},” said
+${protagonist}. “It’s probably best if they all try to get
+closer to the jungle.” They said goodbye and then
+walked back across the savannah.
+
+Now they needed to gather all of the animals
+together to start the long journey. ${protagonist} didn’t
+need to go along because she had plenty of food in
+her high trees. But she was a good friend
+and wanted to help as much as she could.
+They walked towards her favorite trees
+and guess what she and ${pet} saw?
+
+All of the animals were already gathered there and
+waiting for ${protagonist} and ${pet}! “Why are you
+all here?” asked ${protagonist}. “${mom} the Eagle told us
+that you’re taking us to the edge of the jungle,”
+said one of the zebras. “How did he know?” asked
+${pet}. “He heard you talking with ${dad},” said
+one of the ${pet-species}. “So you’ve all decided to
+try to walk together to the jungle?” asked ${protagonist}.
+
+“Yes,” they all answered. “If you’ll help us to get
+there,” added one of the elephants. “Of course,
+I’ll help,” said ${protagonist}. “I’m your friend.”
+So they started on their big journey across the
+savannah to the jungle. The sun was so hot and
+there was no food and no place to get a drink of
+water. And then they heard it. It was thunder!
+
+“Thunder!” they all shouted. Thunder was very
+scary to the animals of the savannah. Can you
+guess why? Fire! Sometimes the lightning hits
+the dry grass and the whole savannah catches fire.
+Then everyone has to run. And that is exactly
+what happened. “Fire!” they shouted. They
+could smell it. And now they could see it.
+The fire was coming towards them!
+
+${protagonist} had to think fast. Because she was so
+tall she could see farther than any of the other
+animals. She stretched her long neck up as far
+as she could. Now she could see a place where
+there was no fire. “Quick!” she said. “Everyone
+run this way.” All of the animals followed her
+and ran as fast as they could. Soon, they were
+out of danger. They stopped and gathered
+together one more time. “Is everyone here?”
+${protagonist} asked. She wanted to make sure they
+were all safe. All the animals looked around;
+they were all there.
+
+And then the most wonderful thing happened.
+“I felt a drop,” said a zebra. “Me too,” said an
+antelope. It was rain! Finally! Everyone was
+very happy. It started to rain a little more and
+then a little more. The rain put out the fire so
+they didn’t have to run. And the rain filled their
+watering holes and made the plants grow again,
+so they didn’t have to make that long trip to the
+edge of the jungle. Everyone would soon have
+enough food and water. So ${protagonist}, the Giraffe
+went back to her favorite tree and continued
+to munch on the leaves and buds.');
+
+insert into story values(3, 'Space Captain', 'Princess ${sister} always has the best birthday parties
+and this year friends from all over the galaxy were
+heading to her planet to enjoy the celebrations.
+Captain ${protagonist} and his wonder ${pet-species} ${pet} had
+picked the perfect present for their inspiring friend.
+However, Captain ${protagonist}’s arch nemesis, the evil
+Doctor ${brother} was determined that there would be no
+birthday cake for the pair at the end of this trip.
+
+The golden Yellow Rocketship stood proudly on the
+launch pad ready for takeoff. Captain ${protagonist} had
+just finished loading Princess ${sister}’s awkwardly long
+birthday present. “That’s it, ${pet}.” he said patting
+his faithful ${pet-species}. “Now I just need to go and return all
+this wrapping paper.”and he headed towards the
+hanger while untangling himself from all the super
+sticky tape he’d used to wrap up the gift.
+
+${pet} gave a happy bark and off Captain
+${protagonist} went into the space hanger. As ${pet}
+returned to the spaceship he thought he saw
+someone walking away from their Rocket Ship.
+He looked familiar; was that Doctor ${brother}?
+Later, in the ship all systems were go. Lights blinked,
+screens lit up and Captain ${protagonist} and ${pet}
+were strapped in ready to go.
+
+“5,4,3,2,1, Blast Off and away we go.” shouted
+Captain ${protagonist}, a big grin on his face whenever
+he headed back out into space. Today ${pet}
+only gave a little whimper though;
+what was that Doctor ${brother} up to?
+On Doctor ${brother}’s great black ship however,
+there was only laughter, the kind only an evil
+genius can produce; deep from the stomach
+and exploding from the mouth.
+“Good luck Captain ${protagonist},
+enjoy your ride.”
+
+Both ships were now busy with the sound of song:
+“Roaring in our Rocket, flying really fast,
+Being in a spaceship is a blast blast blast!”
+sang Captain ${protagonist}.
+
+While on the dark ship of ${brother} his creepy
+chorus rang out:
+“My mean machine is in the socket.
+It will make the Captain Fluster.
+Magnetic power will route his rocket
+To the Crazy Comet Cluster”
+
+“Not long now, ${pet}. Princes ${sister}’s planet is just
+around the next sun.” explained Captain ${protagonist}.
+Out of the window ${pet} stared at the golden
+planets, and the shining suns flying past
+as they hurtled through space.
+
+“WahWahWah”. The cool hum of the spaceship was
+suddenly thrown into chaos as the red warning light
+flashed and the ship suddenly and
+sharply turned around.
+
+“Oh no!” cried Captain ${protagonist}. “We’re going the
+wrong way”. ${pet} looked at the screen and saw
+where they were headed: “Oh no, indeed!”
+The yellow rocket ship was now headed towards
+the Crazy Comet Cluster, the zaniest place in space.
+
+Hundreds of blazing red comets swarmed around a
+golden Yellow sun and if a spaceship got too close,
+they would chase and churn it for hours upon hours.
+${pet} barked and ran to the back of the ship.
+“Don’t worry, boy.” said Captain ${protagonist}. “It will
+be ok….I hope.” as he started pressing buttons and
+fighting with the spaceships steering wheel. Sweat
+broke out on his forehead and his eyes widened
+into two moons of panic.
+
+${pet} was not really scared, however, he was
+searching for whatever the mischievous Doctor ${brother}
+had hidden on their ship. ${pet} knew that it was
+the only reason they were in trouble, but what was it?
+
+Watching from behind a small green moon was
+the shadowy spaceship of Doctor ${brother}. A big smile
+of satisfaction grew on his face as he watched
+the Captain’s ship head towards the Crazy Comet
+Cluster. Once again his evil laugh echoed
+around the gleaming cockpit of his ship.
+“Magnetic force, keep that rocket off course.”
+he whispered to himself.
+
+${pet} knew they were almost out of time when
+he saw it; or rather he sniffed out the evil stench
+of where Doctor ${brother} had been. It was a small
+blinking box just underneath the main computer.
+That had to be it. Doctor ${brother} had made the yellow
+rocket go in the wrong direction using one of
+his mischief machines. Quick as a flash ${pet}
+snatched the box in his jaws and teetering along the
+lengthy birthday gift threw it to the Space Particle
+Accelerator Machine (SPAM for short).He then used
+his paws and sent the device back to Doctor ${brother}’s
+nearby ship. ‘Lets see how much he enjoys those
+crazy comets.’ thought ${pet}.
+
+Back on the bridge Captain ${protagonist} was
+still banging, twisting pushing and pressing
+everything he could, When suddenly the ship turned
+around and headed back to Princess ${sister}’s planet.
+${pet} trotted in and gave a friendly happy bark.
+“I told you not to worry ${pet}; as I say ‘with the
+Captain around a solution is found.’”
+${pet} gave a happy bark before picking up
+his favourite bone and settling back to enjoy the
+remainder of the journey.
+
+A short while later Captain ${protagonist} and ${pet}
+were getting hugs and high fives as Princess ${sister}
+opened up her birthday present.“Anti-Gravity skies!
+“Cool!” she said while up above some crazy comets
+were chasing a rather unhappy Doctor ${brother}.');
