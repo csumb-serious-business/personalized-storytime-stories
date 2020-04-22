dc8fbccd536e32d9bccf1b28308f6ba94eebf455
Raymond Talmage <raymondtalmage@gmail.com>
2020-04-21 23:35:52 -0700

    updated StoryLoad

--- src/main/java/storytime/story/StoryLoad.java
+++ src/main/java/storytime/story/StoryLoad.java
@@ -1,42 +1,24 @@
 package storytime.story;
 
-import org.springframework.beans.factory.BeanCreationException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.data.ConditionalOnRepositoryType;
-import org.springframework.context.annotation.*;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.jdbc.datasource.init.DataSourceInitializer;
 import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
-import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.SQLData;
-import java.sql.SQLException;
+
 
 
 @Configuration
-@ComponentScan
 public class StoryLoad {
 
-    @Bean
-    @Lazy
-    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) {
+   @Bean
+   public ResourceDatabasePopulator resourceDatabasePopulator(@Qualifier("dataSource") DataSource dataSource) {
+       ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator(new ClassPathResource("/import.sql"));
+       resourceDatabasePopulator.setContinueOnError(true);
+       resourceDatabasePopulator.execute(dataSource);
+       return resourceDatabasePopulator;
+   }
 
-        try {
-            ResourceDatabasePopulator resourceDatabasePopulator = new ResourceDatabasePopulator();
-            resourceDatabasePopulator.addScript(new ClassPathResource("/import.sql"));
-            DataSourceInitializer dataSourceInitializer = new DataSourceInitializer();
-            dataSourceInitializer.setDataSource(dataSource);
-            dataSourceInitializer.setDatabasePopulator(resourceDatabasePopulator);
-            return dataSourceInitializer;
-        }
-        catch(Exception e){
-            throw new BeanCreationException("Bean", "super bean", e);
-        }
-    }
 }
